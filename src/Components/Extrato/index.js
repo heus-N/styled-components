c17ee49cabc000947028348ea29b6cf664755085
marciano.ns <marciano.ns@bolsista.pti.org.br>
2024-11-06 10:18:02 -0300

    feat: criação extrato

---
+++ src/Components/Extrato/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Box, Botao } from '../../Components/UI'
+import { extratoLista } from '../../info'
+
+
+export const Extrato = () => {
+  return(
+    <Box>
+      {extratoLista.updates.map(({id, type, from, value, date}) => {
+        return(
+          <div key={id}>
+            <div>
+              {type}
+            </div>
+            <div>
+              {from}
+            </div>
+          </div>
+        )
+      })}
+    <Botao>
+      Ver mais
+    </Botao>
+    </Box>
+
+  )
+}
