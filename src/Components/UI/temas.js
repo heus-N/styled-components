51d379920d2022755f0eaf2b2dc9fbf0d44ef093
marciano.ns <marciano.ns@bolsista.pti.org.br>
2024-11-06 11:14:48 -0300

    feat: criação tema escuro

---
+++ src/Components/UI/temas.js
@@ -0,0 +1,16 @@
+import {
+  fundoClaro, conteudoClaro, textoFundoClaro,
+  fundoEscuro, conteuEscuro, textoFundoEscuro
+} from './variaveis'
+
+export const temaClaro = {
+  body: fundoClaro,
+  inside: conteudoClaro,
+  text: textoFundoClaro
+}
+
+export const temaEscuro = {
+  body: fundoEscuro,
+  inside: conteuEscuro,
+  text: textoFundoEscuro
+}
